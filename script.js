55dfa8c0b4286bb1870c9524d908f17eaa8bed7c
NISH9090 <120647573+NISH9090@users.noreply.github.com>
2024-04-26 14:09:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Automatic Slideshow - change image every 3 seconds
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
